9cfb87a44712da6aacff4df2ad521048b77734cc
VinTran <tranquillo.vinnie@gmail.com>
2019-03-09 10:42:51 -0500

    Added Firebase Connectivity

---
+++ assets/javascript.js
@@ -0,0 +1,12 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBxVnmqd-Xs_7vIlpI4hwpe7qQM1UHetqo",
+    authDomain: "first-project-ad62c.firebaseapp.com",
+    databaseURL: "https://first-project-ad62c.firebaseio.com",
+    projectId: "first-project-ad62c",
+    storageBucket: "first-project-ad62c.appspot.com",
+    messagingSenderId: "1021863862542"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
